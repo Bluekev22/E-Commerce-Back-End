a2b28d4ea80fcc03b97e4f7586f1297f73d983db
Kevin Shank <kevin_shank@yahoo.com>
2021-08-30 13:44:57 -0400

    created delete for category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -47,7 +47,7 @@ router.post("/", async (req, res) => {
   // create a new category
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
   try {
     const categoryData = await Category.update(
@@ -70,8 +70,22 @@ router.put("/:id", (req, res) => {
   }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
+  try{
+    const categoryData = Category.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "No category found with that id!" });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
